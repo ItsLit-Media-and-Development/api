dcf86481384d9495e1ba5f20537e40473c0129f3
Marc Towler <marc@marctowler.co.uk>
2017-12-13 17:56:42 +0000

    Added Questions endpoint Added .htaccess file

---
+++ Application/Controllers/Questions.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MarcT
+ * Date: 06/12/2017
+ * Time: 20:15
+ */
+
+namespace API\Controllers;
+
+error_reporting(E_ALL);
+use API\Library;
+
+class Questions
+{
+    private $_db;
+    private $_config;
+
+    public function __construct()
+    {
+        $tmp           = new Library\Router();
+        $this->_config = new Library\Config();
+        $this->_db     = $this->_config->database();
+        $this->_params = $tmp->getAllParameters();
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
+
+    /**
+     * Endpoint allows user's to add questions. Requires 2 URI parameters, 1) username 2) message
+     * Example URL https://domain.com/Questions/add/$user/$message
+     *
+     * @return string JSON encoded array
+     */
+    public function add()
+    {
+        $user     = $this->_params[0];
+        $question = $this->_params[1];
+        $json     = '';
+
+        if($user != '' && $question != '')
+        {
+            try {
+                $stmt = $this->_db->prepare("INSERT INTO questions (user, question) VALUES (:user, :question)");
+                $stmt->execute([
+                    ':user' => $user,
+                    ':question' => $question
+                ]);
+
+                if ($stmt->rowCount() > 0) {
+                    $json = ['status' => 200, 'response' => "Question added"];
+                }
+            } catch (\PDOException $e) {
+                $json = ["status" => 400, "response" => $e->getMessage()];
+            }
+        }
+
+        return json_encode($json);
+    }
+
+    /**
+     * Endpoint allows user to see list of questions that have been asked within the past 4 hours.
+     * Example URL https://domain.com/Questions/showlist
+     *
+     * @return string
+     */
+    public function showlist()
+    {
+        $stmt = $this->_db->prepare("SELECT user, question, date FROM questions WHERE date > SUBDATE( CURRENT_TIMESTAMP, INTERVAL 4 HOUR ) ORDER BY date ASC");
+        $stmt->execute();
+
+        return json_encode(['status' => 200, 'response' => $stmt->fetchAll(\PDO::FETCH_ASSOC)]);
+    }
+}
