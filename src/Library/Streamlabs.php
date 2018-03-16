852d84e7f0b277abdcb17346e5a445c9bab0a88b
Marc Towler <marc@marctowler.co.uk>
2018-03-16 22:10:36 +0000

    Seperated out the Twitch and TwitchRequest libraries to fix the autoloader issue

---
+++ src/Library/Streamlabs.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Stream Labs Library
+ *
+ * Working with streamelements
+ *
+ * @package       API
+ * @author        Marc Towler <marc@marctowler.co.uk>
+ * @copyright     Copyright (c) 2018 Marc Towler
+ * @license       https://github.com/Design-Develop-Realize/api/blob/master/LICENSE.md
+ * @link          https://api.itslit.uk
+ * @since         Version 1.0
+ * @filesource
+ */
+
+namespace API\Library;
+
+
+class Streamlabs
+{
+
+}
