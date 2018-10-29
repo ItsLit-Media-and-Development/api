aa7ac4353a28ee8f8d8502247b50acbf194fcc4c
Marc Towler <marc@marctowler.co.uk>
2018-10-29 01:14:53 +0000

    Start of the LoL Riot endpoints and library

---
+++ src/Application/Controllers/lol.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * lol.php - API
+ *
+ * @author    Marc Towler <marc.towler@designdeveloprealize.com>
+ *
+ * @copyright 2018 - Design Develop Realize
+ */
+
+namespace API\Controllers;
+
+
+class lol
+{
+
+}
