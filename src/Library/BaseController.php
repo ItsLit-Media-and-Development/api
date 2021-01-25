650fc7cc49489e8fff88b1b8b09f45e3973478ce
Marc Towler <marc@marctowler.co.uk>
2021-01-25 23:11:54 +0000

    Updated Library for Better Authentication and Added Shop Titans Endpoint

--- src/Library/BaseController.php
+++ src/Library/BaseController.php
@@ -7,7 +7,7 @@
  */
 
 namespace API\Library;
-
+use GuzzleHttp\Client;
 
 abstract class BaseController
 {
@@ -15,15 +15,25 @@ abstract class BaseController
 	protected $_output;
 	protected $_log;
 	protected $_router;
-	public $output;
+	protected $_auth;
+	protected $_headers;
+	protected $_guzzle;
+	protected $_config;
+	protected $_requstType;
+	protected $_twitch;
 
 	public function __construct()
 	{
-		$this->_config = new Config();
-		$this->_router = new Router();
-		$this->_params = $this->_router->getAllParameters();
-		$this->_output = new Output();
-		$this->_log = new Logger();
+		$this->_config      = new Config();
+		$this->_router      = new Router();
+		$this->_params      = $this->_router->getAllParameters();
+		$this->_output      = new Output();
+		$this->_log         = new Logger();
+		$this->_auth        = new Authentication();
+		$this->_headers     = $this->_router->getAllHeaders();
+		$this->_guzzle      = new Client();
+		$this->_requestType = $this->_router->getRequestType();
+		$this->_twitch      = new Twitch();
 	}
 
 	public function __destruct()
@@ -39,8 +49,47 @@ abstract class BaseController
 	 */
 	public function main()
 	{
-		$this->_log->set_message("main() Called from " . $_SERVER['REMOTE_ADDR'] . ", returning a 501", "INFO");
+		//$this->_log->set_message("main() Called from " . $_SERVER['REMOTE_ADDR'] . ", returning a 501", "INFO");
 
 		return $this->_output->output(501, "Function not implemented", false);
+	}
+
+	public function authenticate()
+	{
+		if(!isset($this->_headers['user']) || ($this->_auth->validate_token($this->_headers['token'], $this->_headers['user'])['level'] != 4))
+        {
+			//No header, could be QS based. Lets see how big _params is and take the last value
+			if($this->_params[sizeof($this->_params)-1][0] === '?')
+			{
+				$string = explode("&",ltrim($this->_params[sizeof($this->_params)-1], $this->_params[sizeof($this->_params)-1][0]));
+
+				$this->_headers['user']  = explode("=", $string[0])[1];
+				$this->_headers['token'] = explode("=", $string[1])[1];
+
+				if(!isset($this->_headers['token']) || ($this->_auth->validate_token($this->_headers['token'], $this->_headers['user'])['level'] != 4))
+        		{
+					return false;
+				} else {
+					return true;
+				}
+			}
+            $this->_log->set_message("Authentication failed", "ERROR");
+
+			return false;
+        } else {
+			return true;
+		}
+	}
+
+	public function validRequest($valid)
+	{
+		if($this->_requestType !== $valid)
+        {
+            $this->_log->set_message("Request received with invalid HTTP request type", "ERROR");
+
+            return false;
+        } else {
+			return true;
+		}
 	}
 }
