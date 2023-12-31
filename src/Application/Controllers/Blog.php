4a6c6739536c7a763784ec8f72f9570263047e88
Marc Towler <marc@marctowler.co.uk>
2023-12-31 22:22:27 +0000

    Added remainder endpoints for Blog pending testing

--- src/Application/Controllers/Blog.php
+++ src/Application/Controllers/Blog.php
@@ -82,6 +82,29 @@ class Blog extends Library\BaseController
     {
         if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
         if(!$this->expectedVerb('PUT')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        //Quick check to make sure the data is not empty
+        if(!isset($data) || empty($data))
+        {
+            return $this->_output->output(400, "No Data sent", false);
+        }
+
+        $output = $this->_db->updatePost($data);
+
+        //Check to see if we had an error
+        if(!is_bool($output))
+        {
+            return $this->_output->output(500, $output, false);
+        }
+
+        if($output === false)
+        {
+            return $this->_output->output(400, $output, false);
+        }
+
+        return $this->_output->output(200, $output, false);
     }
 
     public function createPost()
@@ -134,12 +157,131 @@ class Blog extends Library\BaseController
     public function approvePost()
     {
         if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
-        if(!$this->expectedVerb('PUT')) { return $this->_output->output(405, "Method Not Allowed", false); }
+        if(!$this->expectedVerb('PATCH')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if(!isset($data['id']) || empty($data))
+        {
+            return $this->_output->output(400, "No Data Sent", false);
+        } 
+        elseif(filter_var($id, FILTER_VALIDATE_INT) === false) 
+        {
+            retrun $this->_output->output(400, "ID Should be Numeric", false);
+        }
+
+        $output = $this->_db->togglePostStatus($data['id']);
+
+        return $this->_output->output(200, $output, false);
     }
 
     public function unapprovePost()
     {
         if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
-        if(!$this->expectedVerb('PUT')) { return $this->_output->output(405, "Method Not Allowed", false); }
+        if(!$this->expectedVerb('PATCH')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if(!isset($data['id']) || empty($data))
+        {
+            return $this->_output->output(400, "No Data Sent", false);
+        } 
+        elseif(filter_var($id, FILTER_VALIDATE_INT) === false) 
+        {
+            retrun $this->_output->output(400, "ID Should be Numeric", false);
+        }
+
+        $output = $this->_db->togglePostStatus($data['id']);
+
+        return $this->_output->output(200, $output, false);
+    }
+
+    public function createComment()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('POST')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        //Quick check to make sure the data is not empty
+        if(!isset($data) || empty($data))
+        {
+            return $this->_output->output(400, "No Data sent", false);
+        }
+
+        $output = $this->_db->addComment($data);
+
+        //Check to see if we had an error
+        if(!is_bool($output))
+        {
+            return $this->_output->output(500, $output, false);
+        }
+
+        if($output === false)
+        {
+            return $this->_output->output(400, $output, false);
+        }
+
+        return $this->_output->output(200, $output, false);
+    }
+
+    public function deleteComment()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('DELETE')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $id = $this->_params[0];
+
+        //Check it is actually a number
+        if(filter_var($id, FILTER_VALIDATE_INT) === false)
+        {
+            return $this->_output->output(400, "Post ID should be numeric", false);
+        }
+
+        $result = $this->_db->deleteComment($id);
+
+        return ($result) ? $this->_output->output(204, "", false) : $this->_output->output(404, "Blog Post not found", false);
+    }
+
+    public function approveComment()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('PATCH')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if(!isset($data['id']) || empty($data))
+        {
+            return $this->_output->output(400, "No Data Sent", false);
+        } 
+        elseif(filter_var($id, FILTER_VALIDATE_INT) === false) 
+        {
+            retrun $this->_output->output(400, "ID Should be Numeric", false);
+        }
+
+        $output = $this->_db->toggleCommentStatus($data['id']);
+
+        return $this->_output->output(200, $output, false);
+    }
+
+    public function unapproveComment()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('PATCH')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if(!isset($data['id']) || empty($data))
+        {
+            return $this->_output->output(400, "No Data Sent", false);
+        } 
+        elseif(filter_var($id, FILTER_VALIDATE_INT) === false) 
+        {
+            retrun $this->_output->output(400, "ID Should be Numeric", false);
+        }
+
+        $output = $this->_db->toggleCommentStatus($data['id']);
+
+        return $this->_output->output(200, $output, false);
     }
 }
