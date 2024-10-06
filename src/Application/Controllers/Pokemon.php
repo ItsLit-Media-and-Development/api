82badf60d24d865dd2238525cd2db9945cf8a943
Marc Towler <marc@marctowler.co.uk>
2024-10-06 18:28:11 +0100

    Added deck endpoints to pokemon

--- src/Application/Controllers/Pokemon.php
+++ src/Application/Controllers/Pokemon.php
@@ -30,10 +30,11 @@ class Pokemon extends Library\BaseController
     //Pull the events that have been stored in the database from the website scrape
     public function listEvents()
     {
-        if(!$this->authenticate(1)) { return $this->_output->output(401, 'Authentication failed', false); }
+        //if(!$this->authenticate(1)) { return $this->_output->output(401, 'Authentication failed', false); }
         if(!$this->expectedVerb('GET')) { return $this->_output->output(405, "Method Not Allowed", false); }
 
-        
+        $test = file_get_contents('https://op-core.pokemon.com/api/v2/event_locator/search/?distance=50&format=api&latitude=53.8007554&longitude=-1.5490774');
+        echo $test;
     }
 
     //Add event, potentially via automation or via manually
@@ -77,5 +78,115 @@ class Pokemon extends Library\BaseController
     {
         if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
         if(!$this->expectedVerb('DELETE')) { return $this->_output->output(405, "Method Not Allowed", false); }
+    }
+
+    public function getDeck()
+    {
+        //if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('GET')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $id = $this->_params[0];
+
+        //Check it is actually a number
+        if(filter_var($id, FILTER_VALIDATE_INT) === false)
+        {
+            return $this->_output->output(400, "Post ID should be numeric", false);
+        }
+
+        $data =  $this->_db->getDeck($id);
+
+        return $this->_output->output(200, $data, false);
+    }
+
+    public function getDecklists()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('GET')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = $this->_db->getDecklists();
+
+        return $this->_output->output(200, $data, false);
+    }
+
+    public function addDecklist()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('POST')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        //Quick check to make sure the data is not empty
+        if(!isset($data) || empty($data))
+        {
+            return $this->_output->output(400, "No Data sent", false);
+        }
+
+        //Check that the minimum data points are present
+        if(!array_key_exists('deck_name', $data) || !array_key_exists('decklist', $data) || 1array_key_exists('season', $data))
+        {
+            return $this->_output->output(400, "Missing data", false);
+        }
+
+        $output = $this->_db->addDecklist($data);
+
+        //Check to see if we had an error
+        if(!is_bool($output))
+        {
+            return $this->_output->output(500, $output, false);
+        }
+
+        if($output === false)
+        {
+            return $this->_output->output(400, $output, false);
+        }
+
+        return $this->_output->output(200, $output, false);
+    }
+
+    public function updateDecklist()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('PUT')) { return $this->_output->output(405, "Method Not Allowed", false); }
+    }
+
+    public function deleteDecklist()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('DELETE')) { return $this->_output->output(405, "Method Not Allowed", false); }
+    }
+
+    public function getTeamlists()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('GET')) { return $this->_output->output(405, "Method Not Allowed", false); }
+    }
+
+    public function updateDeckResult()
+    {
+        if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
+        if(!$this->expectedVerb('PUT')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        //Quick check to make sure the data is not empty
+        if(!isset($data) || empty($data))
+        {
+            return $this->_output->output(400, "No Data sent", false);
+        }
+
+        $output = $this->_db->updateDeckResults($data);
+
+        //Check to see if we had an error
+        if(!is_bool($output))
+        {
+            return $this->_output->output(500, $output, false);
+        }
+
+        if($output === false)
+        {
+            return $this->_output->output(400, $output, false);
+        }
+
+        return $this->_output->output(200, $output, false);
     }
 }
--- src/Application/Model/PokemonModel.php
+++ src/Application/Model/PokemonModel.php
@@ -26,12 +26,23 @@ class PokemonModel extends Library\BaseModel
 
 	public function getEvent(string $id)
 	{
-		$stmt = $this->_db->prepare("SELECT * FROM pkmn_events WHERE sanction_id = :id");
-		$stmt->execute(
-			[
-				':id' => $id
-			]
-		);
+		try
+		{
+			$stmt = $this->_db->prepare("SELECT * FROM pkmn_events WHERE sanction_id = :id");
+			$stmt->execute(
+				[
+					':id' => $id
+				]
+			);
+
+			$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+			return $result;
+		} catch (\Exception $e) {
+			new Exceptions\DBException($e->getMessage());
+
+			return [];
+		}
 	}
 
 	public function getEvents()
@@ -67,5 +78,90 @@ class PokemonModel extends Library\BaseModel
 	public function updateStandings(string $id, array $standings)
 	{
 
+	}
+
+	public function getDecklists()
+	{
+		try
+		{
+			$stmt = $this->_db->prepare("SELECT id, deck_name, decklist, season, played, wins, losses, ties, (wins / (wins + losses + ties)) AS win_percent FROM decklists");
+			$stmt->execute();
+
+			$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+			return $result;
+		} catch (\Exception $e) {
+			new Exceptions\DBException($e->getMessage());
+
+			return [];
+		}
+	}
+
+	public function getDeck(string $id)
+	{
+		try 
+		{
+			$stmt = $this->_db->prepare("SELECT id, deck_name, decklist, season, played, wins, losses, ties, (wins / (wins + losses + ties)) AS win_percent FROM decklists WHERE id = :id");
+			$stmt->execute(
+				[
+					':id' => $id
+				]
+			);
+
+			$result = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+			return $result;
+		} catch (\Exception $e) {
+			new Exceptions\DBException($e->getMessage());
+
+			return [];
+		}
+	}
+
+	public function addDecklist(array $details)
+	{
+		try
+		{
+			$ins = $this->_db->prepare("INSERT INTO decklists (deck_name, decklist, season, played, wins, losses, ties) VALUES(:deckname, :decklist, :season, 0, 0, 0, 0)");
+			$ins->execute(
+				[
+					':deck_name' => $details['deck_name'],
+					':decklist'  => $details['decklist'],
+					':season'    => $details['season']
+				]
+			);
+
+			$this->_output = ($ins->rowCount() > 0) ? true : false;
+        }
+        catch(\PDOException $e)
+        {
+            $this->_output = $e->getMessage();
+        }
+
+        return $this->_output;
+	}
+
+	public function updateDeckResults(array $details)
+	{
+		try 
+		{
+			$upd = $this->_db->prepare("UPDATE decklists SET wins = wins + :win, losses = losses + :loss, ties = ties + :tie, played = 1 WHERE id = :id");
+			$upd->execute(
+				[
+					':id'   => $details['id'],
+					':win'  => $details['win'],
+					':loss' => $details['loss'],
+					':tie'  => $details['tie']
+				]
+			);
+
+				$this->_output = ($upd->rowCount() > 0) ? true : false;
+			}
+			catch(\PDOException $e)
+			{
+				$this->_output = $e->getMessage();
+			}
+	
+			return $this->_output;
 	}
 }
