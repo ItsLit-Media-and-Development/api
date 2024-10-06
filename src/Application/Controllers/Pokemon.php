719ae3182bb8f1d03b8b9c3bd8fa8857d9364329
Marc Towler <marc@marctowler.co.uk>
2024-10-07 00:00:56 +0100

    Added update decklist

--- src/Application/Controllers/Pokemon.php
+++ src/Application/Controllers/Pokemon.php
@@ -147,6 +147,29 @@ class Pokemon extends Library\BaseController
     {
         if(!$this->authenticate(4)) { return $this->_output->output(401, 'Authentication failed', false); }
         if(!$this->expectedVerb('PUT')) { return $this->_output->output(405, "Method Not Allowed", false); }
+
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        //Quick check to make sure the data is not empty
+        if(!isset($data) || empty($data))
+        {
+            return $this->_output->output(400, "No Data sent", false);
+        }
+
+        $output = $this->_db->updateDecklist($data);
+
+        //Check to see if we had an error
+        if(!is_bool($output))
+        {
+            return $this->_output->output(500, $output, false);
+        }
+
+        if($output === false)
+        {
+            return $this->_output->output(400, $output, false);
+        }
+
+        return $this->_output->output(200, $output, false);
     }
 
     public function deleteDecklist()
